63a5dd27e3b557446fc8df1b1dbbf052d96425f1
Mauritz Langer <mauritz@langer-net.de>
2023-01-06 14:38:05 +0100

    Inital Commit of a Spring Project

---
+++ src/main/java/com/highsix/versioncontrol/VersionControlApplication.java
@@ -0,0 +1,13 @@
+package com.highsix.versioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/highsix/versioncontrol/VersionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highsix.versioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

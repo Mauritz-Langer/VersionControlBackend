f602439550f4cabe818e0345817e6e7a8b4599ab
Mauritz Langer <mauritz@langer-net.de>
2023-01-08 13:15:19 +0100

    Include SLF4J Logging with ConsoleHandler and FileHandler

--- src/main/java/com/highsix/versioncontrol/VersionControlApplication.java
+++ src/main/java/com/highsix/versioncontrol/VersionControlApplication.java
@@ -3,11 +3,29 @@ package com.highsix.versioncontrol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
 @SpringBootApplication
 public class VersionControlApplication {
 
+    private static LogManager logManager = LogManager.getLogManager();
+
     public static void main(String[] args) {
         SpringApplication.run(VersionControlApplication.class, args);
+
+        configureLogging();
+    }
+
+    private static void configureLogging(){
+        try {
+            logManager.readConfiguration(new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/logging.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }

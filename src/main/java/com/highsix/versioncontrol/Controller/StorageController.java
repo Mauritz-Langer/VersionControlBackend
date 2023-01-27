aa33af72ea0734f3ac1ea6f9b2d75a1fb90dffe8
Mauritz Langer <mauritz5022@gmail.com>
2023-01-27 10:55:01 +0100

    Include Log Messages in the code

--- src/main/java/com/highsix/versioncontrol/Controller/StorageController.java
+++ src/main/java/com/highsix/versioncontrol/Controller/StorageController.java
@@ -27,10 +27,13 @@ public class StorageController {
 
     @PostMapping("addFile")
     public TextFile addFile(@RequestBody TextFile textFile) {
-        log.info("REST request to upload file");
+        log.info("'addFile'-Endpoint was triggered");
+        log.debug("Content of the 'addFile'-Request: " + textFile);
         try {
             firebaseStorage.uploadFile(textFile);
+            log.info("'addFile'-Request was successfully processed");
         } catch (Exception e) {
+            log.error(e.getMessage());
             return null;
         }
         return textFile;
@@ -39,27 +42,36 @@ public class StorageController {
 
     @GetMapping("getAllFiles")
     public TextFile[] getAllFiles() {
+        log.info("'getAllFiles'-Endpoint was triggered");
         return firebaseStorage.downloadAllFiles();
     }
 
 
     @PostMapping("addVersionAndUnlockFile")
     public TextFile addVersionAndUnlockFile(@RequestBody TextFile file, @RequestParam String versionContent) {
+        log.info("'addVersionAndUnlockFile'-Endpoint was triggered");
+        log.debug("Content of the 'addVersionAndUnlockFile'-Request: " + file + "and Version Content: " + versionContent);
         return firebaseStorage.addVersionAndUnlockFile(file,versionContent);
     }
 
     @PostMapping("lockFile")
     public TextFile lockFile(@RequestBody TextFile file) {
+        log.info("'lockFile'-Endpoint was triggered");
+        log.debug("Content of the 'lockFile'-Request: " + file);
         return firebaseStorage.lockOrUnlockFile(file, true);
     }
 
     @PostMapping("unlockFile")
     public TextFile unlockFile(@RequestBody TextFile file) {
+        log.info("'unlockFile'-Endpoint was triggered");
+        log.debug("Content of the 'unlockFile'-Request: " + file);
         return firebaseStorage.lockOrUnlockFile(file, false);
     }
 
     @PostMapping("resetFileToFormerVersion/{versionId}")
     public TextFile resetFileToFormerVersion(@RequestBody TextFile file, @PathVariable int versionId) {
+        log.info("'resetFileToFormerVersion'-Endpoint was triggered");
+        log.debug("Content of the 'resetFileToFormerVersion'-Request: " + file + "and the Version ID: " + versionId);
         return firebaseStorage.resetFileToFormerVersion(file, versionId);
     }
 
--- src/main/java/com/highsix/versioncontrol/Service/FirebaseStorage.java
+++ src/main/java/com/highsix/versioncontrol/Service/FirebaseStorage.java
@@ -33,6 +33,8 @@ public class FirebaseStorage {
     @PostConstruct
     public void initialize() throws IOException {
 
+        log.info("Startup the Google Cloud Storage Services");
+
         FileInputStream serviceAccount = new FileInputStream(System.getProperty("user.dir")+"/serviceaccount.json");
 
         storageOptions = StorageOptions
@@ -41,25 +43,37 @@ public class FirebaseStorage {
                 .setCredentials(GoogleCredentials.fromStream
                         (serviceAccount))
                 .build();
+
+        log.info("Finished successfully the startup of the Google Cloud Storage Services");
     }
 
 
     public void uploadFile(TextFile textFile) throws Exception {
+        log.debug("'uploadFile'-Methode was triggered with content: " + textFile);
         if (!Arrays.stream(downloadAllFiles()).anyMatch(e -> e.getName().equals(textFile.getName()))){
+            log.info("Found no file that matches the input textFile");
             log.debug("bucket name====" + bucketName);
             for (FileVersion version: textFile.getVersions()) {
                 String objectName = generateFileName(textFile, version);
+                log.info("New Filename was generated: " + objectName);
 
                 Map<String, String> metadata = new HashMap<>();
                 String time = LocalDateTime.now().toString();
                 metadata.put("createdAt", time);
+                log.debug("createdAt: " + time);
                 metadata.put("lastUpdatedAt", time);
+                log.debug("lastUpdatedAt: " + time);
                 metadata.put("versionId", String.valueOf(version.getVersionId()));
+                log.debug("versionId: " + version.getVersionId());
                 metadata.put("locked", "false");
+                log.debug("locked: false");
 
                 Storage storage = storageOptions.getService();
+                log.debug("Storage was initialized");
 
+                log.info("Calling 'createFileInFirebase'-Methode with file-content: " + version.getFileContent());
                 createFileInFirebase(objectName, metadata, storage, version.getFileContent());
+                log.info("Finished 'createFileInFirebase'-Methode successfully");
 
                 log.info("File uploaded to bucket " + bucketName + " as " + objectName);
             }
@@ -71,25 +85,34 @@ public class FirebaseStorage {
     }
 
     public TextFile[] downloadAllFiles() {
+        log.debug("'downloadAllFiles'-Methode was called");
         Storage storage = storageOptions.getService();
         List<TextFile> files = new ArrayList<>();
         Bucket bucket = storage.get(bucketName);
+        log.debug("Bucket was defined: "+ bucketName);
         // List all files in the directory
         Page<Blob> blobs = bucket.list();
+        log.debug("Blob with all entrys was created: " + blobs.getValues());
 
         for (Blob b : blobs.iterateAll()) {
             // Download each file
             byte[] fileBytes = b.getContent();
+            log.debug("Content: " + Arrays.toString(b.getContent()));
             String fileName = b.getName();
+            log.debug("filename: " + b.getName());
             String fileContent = new String(fileBytes);
             Map<String, String> metadata = b.getMetadata();
             List<FileVersion> versions = new ArrayList<>();
             try {
                 TextFile file = files.stream().filter(e -> e.getName().split("€")[0].equals(fileName.split("€")[0])).findFirst().get();
+                log.info("Found file with same Filename");
                 FileVersion latestVersion = getLatestFileVersion(file);
+                log.debug("Found latest Version: " + latestVersion);
                 file.getVersions().add(new FileVersion(latestVersion.getVersionId()+1, fileContent, metadata.get("lastUpdatedAt")));
                 file.setVersionCount(file.getVersionCount()+1);
+                log.info("New Version was created");
             } catch (Exception e){
+                log.info("No File with the Filename was foung");
                 versions.add(new FileVersion(
                         Integer.parseInt(metadata.get("versionId")),
                         fileContent,
@@ -104,6 +127,7 @@ public class FirebaseStorage {
                                 Boolean.parseBoolean(metadata.get("locked"))
                         )
                 );
+                log.info("Created new File with one version");
             }
         }
         return files.toArray(new TextFile[files.size()]);
@@ -126,21 +150,30 @@ public class FirebaseStorage {
         textFile.setVersionCount(newVersionId);
         textFile.setLocked(false);
 
+        log.info("New Version was created");
+
         Storage storage = storageOptions.getService();
         Bucket bucket = storage.get(bucketName);
 
         Map<String, String> versionMetaData = new HashMap<>();
         versionMetaData.put("createdAt", textFile.getCreatedAt());
+        log.debug("createAt: " + textFile.getCreatedAt());
         versionMetaData.put("lastUpdatedAt", time);
+        log.debug("lastUpdatedAt: " + time);
         versionMetaData.put("versionId", String.valueOf(newVersionId));
+        log.debug("versionId: " + newVersionId);
         versionMetaData.put("locked", "false");
+        log.debug("locked: false");
 
+        log.info("Calling 'createFileInFirebase'-Methode with file-content: " + versionContent);
         createFileInFirebase(generateFileName(textFile, getLatestFileVersion(textFile)), versionMetaData, storage, versionContent);
+        log.info("'createFileInFirebase'-Methode was processed successfully");
 
         Page<Blob> blobs = bucket.list();
 
         Map<String, String> updateLockMetaData = new HashMap<>();
         updateLockMetaData.put("locked", "false");
+        log.info("locked: false");
 
         Storage.BlobTargetOption precondition = Storage.BlobTargetOption.generationMatch();
 
@@ -149,6 +182,7 @@ public class FirebaseStorage {
                 element.toBuilder().setMetadata(updateLockMetaData).build().update(precondition);
             }
         }
+        log.info("Switch locked to false of the file");
 
         return textFile;
     }
@@ -158,19 +192,25 @@ public class FirebaseStorage {
         Bucket bucket = storage.get(bucketName);
 
         Page<Blob> blobs = bucket.list();
+        log.debug("Blobs: " + blobs.getValues());
 
         Map<String, String> updateLockMetaData = new HashMap<>();
         updateLockMetaData.put("locked", String.valueOf(lock));
+        log.debug("locked: " + lock);
 
         Storage.BlobTargetOption precondition = Storage.BlobTargetOption.generationMatch();
 
         for (Blob element : blobs.iterateAll()) {
             if (element.getName().split("€")[0].equals(textFile.getName())) {
+                log.debug("Version with right name was found");
                 element.toBuilder().setMetadata(updateLockMetaData).build().update(precondition);
+                log.debug("Lock status of Version was updated");
             }
         }
 
         textFile.setLocked(lock);
+
+        log.info("File lock status was updated");
 
         return textFile;
     }
@@ -180,6 +220,7 @@ public class FirebaseStorage {
         String time = LocalDateTime.now().toString();
 
         FileVersion copyedFileVersion = getVersionByID(textFile,versionId);
+        log.debug("Version to copy was found: " + copyedFileVersion);
         textFile.getVersions().add(
                 new FileVersion(
                         getLatestFileVersion(textFile).getVersionId()+1,
@@ -187,6 +228,7 @@ public class FirebaseStorage {
                         time
                 )
         );
+        log.info("File was reset to former version");
 
         textFile.setVersionCount(textFile.getVersionCount()+1);
 
@@ -196,13 +238,19 @@ public class FirebaseStorage {
 
         Map<String, String> metadata = new HashMap<>();
         metadata.put("createdAt", textFile.getCreatedAt());
+        log.debug("createAt: " + textFile.getCreatedAt());
         metadata.put("lastUpdatedAt", time);
+        log.debug("lastUpdatedAt: " + time);
         metadata.put("versionId", String.valueOf(latestVersion.getVersionId()));
+        log.debug("versionId: " + latestVersion.getVersionId());
         metadata.put("locked", "false");
+        log.debug("locked: false");
 
         Storage storage = storageOptions.getService();
 
-        createFileInFirebase(objectName, metadata, storage, latestVersion.getFileContent());
+        log.info("Calling 'createFileInFirebase'-Methode with file-content: " + latestVersion.getFileContent());
+        createFileInFirebase(generateFileName(textFile, getLatestFileVersion(textFile)), metadata, storage, latestVersion.getFileContent());
+        log.info("'createFileInFirebase'-Methode was processed successfully");
 
         return textFile;
     }
@@ -210,19 +258,12 @@ public class FirebaseStorage {
 
     /*------------------------------------------------UTILS----------------------------------------------------------*/
 
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        FileOutputStream fos = new FileOutputStream(convertedFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convertedFile;
-    }
-
     public void createFileInFirebase(String fileName, Map<String, String> metadata, Storage storage, String content){
         BlobId blobId = BlobId.of(bucketName, fileName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setMetadata(metadata).build();
 
-        Blob blob = storage.create(blobInfo, content.getBytes());
+        storage.create(blobInfo, content.getBytes());
+        log.info("Created File in Firebase");
     }
 
     private String generateFileName(TextFile textFile, FileVersion version) {
@@ -235,6 +276,7 @@ public class FirebaseStorage {
         for (int k = 0; k < textFile.getVersions().size(); k++) {
             if (textFile.getVersions().get(k).getVersionId() > textFile.getVersions().get(maxIndex).getVersionId()) {
                 maxIndex = k;
+                log.debug("maxFileVersionIndex = " + maxIndex);
             }
         }
         return textFile.getVersions().get(maxIndex);
@@ -242,8 +284,12 @@ public class FirebaseStorage {
 
     private FileVersion getVersionByID(TextFile textFile, int versionId){
         for (FileVersion element : textFile.getVersions()) {
-            if (element.getVersionId() == versionId) return element;
+            if (element.getVersionId() == versionId){
+                log.debug("Version was found by ID");
+                return element;
+            }
         }
+        log.error("No version was found");
         return null;
     }
 }
